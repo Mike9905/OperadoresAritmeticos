1e73a7665382210a1414eaad1dc1953df80caff8
mikew <miguel.galvan@uabc.edu.mx>
2022-03-06 20:41:56 -0800

    Add project files.

---
+++ Suma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 8;
+            double num2 = 4.5;
+            double resultado;
+
+            String saludo = "Hola";
+            String Nombre = "Hugo";
+
+            resultado = num1 + 22;
+            //Console.WriteLine(resultado);
+            Console.WriteLine(saludo + " " + Nombre + " " + "Como estas?");
+            Console.ReadKey();
+        }
+    }
+}
